9f0c614a81bd73b2e451a59f9285667f852129c3
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-02-10 12:57:32 -0800

    additional coverage for macros

--- error/src/macros.rs
+++ error/src/macros.rs
@@ -254,33 +254,100 @@ macro_rules! map_err {
 #[cfg(test)]
 mod test {
 	use crate as bmw_err;
-	use crate::ErrKind;
+	use crate::{ErrKind, Error, ErrorKind};
 	use std::convert::TryInto;
 	use std::fs::File;
 	use std::num::TryFromIntError;
 
+	fn test_kind(k: ErrKind, s: &str, error: Error) -> Result<(), Error> {
+		let err: bmw_err::Error = err!(k, s);
+		let err_kind = err.kind();
+		println!("error.kind={:?},err_kind={:?}", error.kind(), err_kind);
+		assert_eq!(error.kind(), err_kind);
+		Ok(())
+	}
+
 	#[test]
 	fn test_ekinds() -> Result<(), crate::Error> {
-		let err: bmw_err::Error = err!(bmw_err::ErrKind::Configuration, "anything");
-		let _err_kind = err.kind();
-		let raw: bmw_err::Error =
-			bmw_err::ErrorKind::Configuration("configuration error".to_string()).into();
-		assert!(matches!(raw.kind(), _err_kind));
+		let s = "s";
+		let ss = "s".to_string();
+
+		test_kind(ErrKind::Http, s, ErrorKind::Http(ss.clone()).into())?;
+		test_kind(ErrKind::Crypt, s, ErrorKind::Crypt(ss.clone()).into())?;
+		test_kind(ErrKind::Rustls, s, ErrorKind::Rustls(ss.clone()).into())?;
+		test_kind(ErrKind::Errno, s, ErrorKind::Errno(ss.clone()).into())?;
+		test_kind(
+			ErrKind::SystemTime,
+			s,
+			ErrorKind::SystemTime(ss.clone()).into(),
+		)?;
+		test_kind(
+			ErrKind::OperationNotSupported,
+			s,
+			ErrorKind::OperationNotSupported(ss.clone()).into(),
+		)?;
+		test_kind(ErrKind::Alloc, s, ErrorKind::Alloc(ss.clone()).into())?;
+		test_kind(
+			ErrKind::ThreadPanic,
+			s,
+			ErrorKind::ThreadPanic(ss.clone()).into(),
+		)?;
+		test_kind(ErrKind::Overflow, s, ErrorKind::Overflow(ss.clone()).into())?;
+		test_kind(ErrKind::Test, s, ErrorKind::Test(ss.clone()).into())?;
+		test_kind(
+			ErrKind::IllegalState,
+			s,
+			ErrorKind::IllegalState(ss.clone()).into(),
+		)?;
+		test_kind(
+			ErrKind::CorruptedData,
+			s,
+			ErrorKind::CorruptedData(ss.clone()).into(),
+		)?;
+		test_kind(ErrKind::Misc, s, ErrorKind::Misc(ss.clone()).into())?;
+		test_kind(ErrKind::Poison, s, ErrorKind::Poison(ss.clone()).into())?;
+		test_kind(
+			ErrKind::IllegalArgument,
+			s,
+			ErrorKind::IllegalArgument(ss.clone()).into(),
+		)?;
+		test_kind(
+			ErrKind::CapacityExceeded,
+			s,
+			ErrorKind::CapacityExceeded(ss.clone()).into(),
+		)?;
+		test_kind(ErrKind::Timeout, s, ErrorKind::Timeout(ss.clone()).into())?;
+		test_kind(
+			ErrKind::ArrayIndexOutOfBounds,
+			s,
+			ErrorKind::ArrayIndexOutOfBounds(ss.clone()).into(),
+		)?;
+		test_kind(ErrKind::Utf8, s, ErrorKind::Utf8(ss.clone()).into())?;
+		test_kind(
+			ErrKind::UnexpectedEof,
+			s,
+			ErrorKind::UnexpectedEof(ss.clone()).into(),
+		)?;
+		test_kind(ErrKind::Log, s, ErrorKind::Log(ss.clone()).into())?;
+		test_kind(ErrKind::IO, s, ErrorKind::IO(ss.clone()).into())?;
+		test_kind(
+			ErrKind::Configuration,
+			s,
+			ErrorKind::Configuration(ss.clone()).into(),
+		)?;
+
+		Ok(())
+	}
+
+	fn test_map(err_kind: ErrKind, _error_kind: ErrorKind) -> Result<(), Error> {
+		let map: Result<usize, Error> = map_err!((-1).try_into(), err_kind);
+		let kind = map.unwrap_err().kind();
+		assert_eq!(kind, _error_kind);
 		Ok(())
 	}
 
 	#[test]
 	fn test_map_err() -> Result<(), crate::Error> {
-		let res = map_err!(
-			File::open("/path/to/nothing"),
-			bmw_err::ErrKind::Configuration,
-			"caused by"
-		);
-		assert!(matches!(
-			res.as_ref().unwrap_err().kind(),
-			crate::ErrorKind::Configuration(_),
-		));
-
 		let res = map_err!(
 			File::open("/path/to/nothing"),
 			bmw_err::ErrKind::Log,
@@ -311,6 +378,58 @@ mod test {
 		let kind = map.unwrap_err().kind();
 		let _arg = crate::ErrorKind::IllegalArgument("".to_string());
 		assert!(matches!(kind, _arg));
+
+		let s = ": out of range integral type conversion attempted".to_string();
+		test_map(ErrKind::Http, ErrorKind::Http(s.clone()).into())?;
+		test_map(
+			ErrKind::Configuration,
+			ErrorKind::Configuration(s.clone()).into(),
+		)?;
+		test_map(ErrKind::IO, ErrorKind::IO(s.clone()).into())?;
+		test_map(ErrKind::Log, ErrorKind::Log(s.clone()).into())?;
+		test_map(
+			ErrKind::UnexpectedEof,
+			ErrorKind::UnexpectedEof(s.clone()).into(),
+		)?;
+		test_map(ErrKind::Utf8, ErrorKind::Utf8(s.clone()).into())?;
+		test_map(
+			ErrKind::ArrayIndexOutOfBounds,
+			ErrorKind::ArrayIndexOutOfBounds(s.clone()).into(),
+		)?;
+		test_map(ErrKind::Timeout, ErrorKind::Timeout(s.clone()).into())?;
+		test_map(
+			ErrKind::CapacityExceeded,
+			ErrorKind::CapacityExceeded(s.clone()).into(),
+		)?;
+		test_map(
+			ErrKind::IllegalArgument,
+			ErrorKind::IllegalArgument(s.clone()).into(),
+		)?;
+		test_map(ErrKind::Poison, ErrorKind::Poison(s.clone()).into())?;
+		test_map(ErrKind::Misc, ErrorKind::Misc(s.clone()).into())?;
+		test_map(
+			ErrKind::CorruptedData,
+			ErrorKind::CorruptedData(s.clone()).into(),
+		)?;
+		test_map(
+			ErrKind::IllegalState,
+			ErrorKind::IllegalState(s.clone()).into(),
+		)?;
+		test_map(ErrKind::Test, ErrorKind::Test(s.clone()).into())?;
+		test_map(ErrKind::Overflow, ErrorKind::Overflow(s.clone()).into())?;
+		test_map(
+			ErrKind::ThreadPanic,
+			ErrorKind::ThreadPanic(s.clone()).into(),
+		)?;
+		test_map(ErrKind::Alloc, ErrorKind::Alloc(s.clone()).into())?;
+		test_map(
+			ErrKind::OperationNotSupported,
+			ErrorKind::OperationNotSupported(s.clone()).into(),
+		)?;
+		test_map(ErrKind::SystemTime, ErrorKind::SystemTime(s.clone()).into())?;
+		test_map(ErrKind::Errno, ErrorKind::Errno(s.clone()).into())?;
+		test_map(ErrKind::Rustls, ErrorKind::Rustls(s.clone()).into())?;
+		test_map(ErrKind::Crypt, ErrorKind::Crypt(s.clone()).into())?;
 
 		Ok(())
 	}
