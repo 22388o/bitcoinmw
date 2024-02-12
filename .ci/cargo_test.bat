eb6f28d2b0fab105f339aa88c72d45ac510c03eb
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-02-11 16:49:56 -0800

    fix batch files on fail

--- .ci/cargo_test.bat
+++ .ci/cargo_test.bat
@@ -1,8 +1,10 @@
-git show --summary | findstr "Pipelines-Bot" > tmp.txt
+@echo OFF
+git show --summary | findstr "Author" | findstr "Pipelines-Bot" > tmp.txt
 set /p VAR11=<tmp.txt
-IF [%VAR11%]==[] (
+IF "%VAR11%" equ "" (
   rustup update
   cargo test --all --jobs 1
 )
 set "VAR11="
 del tmp.txt
+@echo ON
--- .ci/perf.bat
+++ .ci/perf.bat
@@ -1,9 +1,11 @@
-git show --summary | findstr "Pipelines-Bot" > tmp.txt
+@echo OFF
+git show --summary | findstr "Author" | findstr "Pipelines-Bot" > tmp.txt
 set /p VAR11=<tmp.txt
-IF [%VAR11%]==[] (
+IF "%VAR11%" equ "" (
   cd etc\evh_perf
   cargo build --release --jobs 1
   target\release\evh_perf -e -c -i 100 --count 10 --clients 2 -t 10
 )
 set "VAR11="
 del tmp.txt
+@echo ON
