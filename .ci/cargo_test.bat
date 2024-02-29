208bb22fd74d50ddb9c9bff7781c478ad8eb8fc6
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-02-29 08:52:55 -0800

    check for errors correctly

--- .ci/cargo_test.bat
+++ .ci/cargo_test.bat
@@ -3,28 +3,22 @@ git show --summary | findstr "Author" | findstr "Pipelines-Bot" > tmp.txt
 set /p VAR11=<tmp.txt
 IF "%VAR11%" equ "" (
   rustup update
-  set ERRORLEVEL=0
   cargo test --all --jobs 1
-  echo "e=%ERRORLEVEL%"
-  IF %ERRORLEVEL% GEQ 1 (
+  IF errorlevel 1 (
     EXIT /B 2
   )
 ) ELSE (
   IF "%1" equ "Schedule" (
     rustup update
-    set ERRORLEVEL=0
     cargo test --all --jobs 1
-    echo "e=%ERRORLEVEL%"
-    IF %ERRORLEVEL% GEQ 1 (
+    IF errorlevel 1 (
       EXIT /B 2
     )
   ) ELSE (
     IF "%1" equ "Manual" (
       rustup update
-      set ERRORLEVEL=0
       cargo test --all --jobs 1
-      echo "e=%ERRORLEVEL%"
-      IF %ERRORLEVEL% GEQ 1 (
+      IF errorlevel 1 (
         EXIT /B 2
       )
     )
