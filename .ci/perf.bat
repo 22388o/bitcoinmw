5928ef3225de42d81b467a28687ff71458f7fb83
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-02-29 08:54:05 -0800

    check perf correctly

--- .ci/perf.bat
+++ .ci/perf.bat
@@ -4,27 +4,24 @@ set /p VAR11=<tmp.txt
 IF "%VAR11%" equ "" (
   cd etc\evh_perf
   cargo build --release --jobs 1
-  set ERRORLEVEL=0
   target\release\evh_perf -e -c -i 100 --count 10 --clients 2 -t 10
-  IF %ERRORLEVEL% GEQ 1 (
+  IF errorlevel 1 (
     EXIT /B 2
   )
 ) ELSE (
   IF "%1" equ "Schedule" (
     cd etc\evh_perf
     cargo build --release --jobs 1
-    set ERRORLEVEL=0
     target\release\evh_perf -e -c -i 100 --count 10 --clients 2 -t 10 
-    IF %ERRORLEVEL% GEQ 1 (
+    IF errorlevel 1 (
       EXIT /B 2
     )
   ) ELSE (
       IF "%1" equ "Manual" (
         cd etc\evh_perf
         cargo build --release --jobs 1
-	set ERRORLEVEL=0
         target\release\evh_perf -e -c -i 100 --count 10 --clients 2 -t 10
-	IF %ERRORLEVEL% GEQ 1 (
+	IF errorlevel 1 (
 	  EXIT /B 2
         )
       )
