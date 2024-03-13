71e1ff793689ea4c3564cf3ea1d2a516f1e4ec62
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-03-13 08:25:12 -0700

    decrease messages

--- evh/src/test.rs
+++ evh/src/test.rs
@@ -2464,7 +2464,7 @@ mod test {
 		let server_received_abc_clone = server_received_abc.clone();
 
 		let mut big_msg = vec![];
-		big_msg.resize(10_000 * 1024, 7u8);
+		big_msg.resize(10 * 1024, 7u8);
 		big_msg[0] = 't' as u8;
 		let big_msg_clone = big_msg.clone();
 		let mut server_accumulator = lock_box!(vec![])?;
