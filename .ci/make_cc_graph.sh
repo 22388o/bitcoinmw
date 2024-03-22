77470dc40d1a3cbc9dd6da87b4928483670d8ef8
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-03-21 22:59:13 -0700

    update percentages

--- .ci/make_cc_graph.sh
+++ .ci/make_cc_graph.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp docs/code_coverage.html.template docs/code_coverage.html;
+cp lib.rs.template src/lib.rs;
 # copy tarpaulin output into template
 export tarpaulin_output=`cat /tmp/tarpaulin.out`;
 perl -pi -e 's/REPLACETARPAULINOUTPUT/$ENV{tarpaulin_output}/g' docs/code_coverage.html
@@ -10,6 +11,7 @@ entries=`cat docs/tarpaulin_summary.txt`;
 declare -a timestamps;
 declare -a values;
 i=0;
+last_value=0.0;
 rm -f /tmp/timestamps
 rm -f /tmp/values
 for entry in $entries
@@ -18,6 +20,7 @@ do
 	then
 		echo "format_date($entry * 1000 )," >> /tmp/timestamps
 	else
+		last_value = $entry;
 		echo "$entry," >> /tmp/values
 	fi
 	let i=i+1;
@@ -25,6 +28,9 @@ done
 
 # update our template with real values
 export coverage=`cat /tmp/values`;
+export last_entry="$last_value";
 perl -pi -e 's/REPLACECOVERAGE/$ENV{coverage}/g' docs/code_coverage.html
+perl -pi -e 's/REPLACECOVERAGE_SINGLE/$ENV{last_entry}/g' docs/code_coverage.html
+perl -pi -e 's/REPLACECOVERAGE_SINGLE/$ENV{last_entry}/g' src/lib.rs
 export timestampsv=`cat /tmp/timestamps`;
 perl -pi -e 's/REPLACETIMESTAMP/$ENV{timestampsv}/g' docs/code_coverage.html
