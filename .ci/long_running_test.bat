9f5e87889f401977ae05e51e24bde9ef7121624e
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-03-28 22:45:17 -0700

    windows long running test

---
+++ .ci/long_running_test.bat
@@ -0,0 +1,14 @@
+@echo OFF
+IF "%1" equ "Schedule" (
+  cargo install cargo-tarpaulin
+  for /L %%a in (1,1,100) do (
+    echo "Running tests: %%a"
+    cargo tarpaulin -p bmw_evh2 --skip-clean
+    IF errorlevel 1 (
+      EXIT /B 2
+    )
+  )
+) ELSE (
+  echo "not running, not the nightly build";
+)
+@echo ON
