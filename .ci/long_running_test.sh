bc90502c0ede0eb71a8d9bc4b91e4b7a02cac843
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-03-29 10:50:49 -0700

    fix loops

--- .ci/long_running_test.sh
+++ .ci/long_running_test.sh
@@ -2,8 +2,10 @@
 
 if [ "$1" == "Schedule" ]; then
   cargo install cargo-tarpaulin
+  LOOPS=$(($2 + 0))
+  echo $LOOPS
 
-  for i in {0..$2}
+  for (( i=0; i<$LOOPS; i++ ))
   do
     echo "Running tests: $i `date`"
     cargo tarpaulin -p bmw_evh2 --skip-clean
