0eb073d189e411639f1065d61a42a742edfdc51b
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-04-02 15:12:37 -0700

    kick off tests with hour limit

--- .ci/long_running_test.sh
+++ .ci/long_running_test.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+start=`date +%s`
+
 if [ "$1" == "Schedule" ]; then
   cargo install cargo-tarpaulin
-  LOOPS=$(($2 + 0))
+  LOOPS=10000;
   echo $LOOPS
 
   for (( i=0; i<$LOOPS; i++ ))
   do
+    now=`date +%s`
+    diff=$(($now - $start))
+    echo "diff=$diff"
     echo "Running tests: $i `date`"
+    if [ $diff -gt $2 ]; then
+      echo "breaking diff=$diff,limit=$2";
+      break;
+    fi
     cargo tarpaulin -p bmw_evh --skip-clean
     if [ $? != 0 ]; then
       break;
