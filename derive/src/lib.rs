da8d37491209d8a5e4ef6c8ce712158273f658d9
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-04-16 14:42:27 -0700

    document derive crate

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -184,6 +184,51 @@ pub fn derive_configurable(strm: TokenStream) -> TokenStream {
 	do_derive_configurable(strm)
 }
 
+/// The [`crate::document()`] proc_macro_attribute is used to document traits and macros. While the
+/// work is done by this macro, all that needs to be done to use it is to place it before a macro
+/// or a trait. All of what it does depends on the [`crate::add_doc()`] attribute. See that
+/// attribute for details on how to use the document attribute. Below are two simple examples
+/// showing where to place the document attribute.
+///
+///```
+/// // trait example
+/// use bmw_err::*;
+/// use bmw_derive::*;
+///
+/// #[document]
+/// pub trait mytrait {
+///     /// This is the write function.
+///     /// more comments.
+///     /// ...
+///     #[add_doc(doc_point)]
+///     /// # Example
+///     ///```
+///     /// myvalue.write();
+///     ///```
+///     #[add_doc(see: "bmw_log::Log::log")]
+///     #[add_doc(error: "bmw_err::ErrKind::IO" - "if an i/o error occurs while writing")]
+///     #[add_doc(input: data - "data to write")]
+///     #[add_doc(return: "this function returns the [`unit`] primitive")]
+///     fn write(&mut self, data: &[u8]) -> Result<(), Error>;
+/// }   
+///```
+///
+///```
+/// // macro example
+/// use bmw_err::*;
+/// use bmw_derive::*;
+///
+/// #[document]
+/// #[add_doc(input: values - "value to add to 1", " usize ")]
+/// #[macro_export]
+/// macro_rules! add_to_1 {
+///        ($name:expr) => {
+///                 let value = $name as usize + 1;
+///                 println!("value: {}", value);
+///        };
+/// }
+///```
+///
 #[proc_macro_attribute]
 pub fn document(attr: TokenStream, item: TokenStream) -> TokenStream {
 	match do_derive_document(attr, item.clone()) {
@@ -198,6 +243,135 @@ pub fn document(attr: TokenStream, item: TokenStream) -> TokenStream {
 	}
 }
 
+/// The [`crate::add_doc()`] proc_macro_attribute is used as a marker to let the [`crate::document()`]
+/// proc_macro_attribute know what to document. It is important to note that currently, these
+/// proc_macro_attributes only support documenting traits and macros. That is because in BitcoinMW,
+/// the public interfaces are typically exposed through traits and macros. Using these
+/// proc_macro_attributes on anything else will result in undefined behaviour.
+///
+/// The [`crate::add_doc()`] proc_macro_attribute takes a parameter that has a specific syntax
+/// needed to document code. The basic form of this attribute is as follows:
+///```text
+/// #[add_doc(<command>: <arguments>]
+///```
+///
+/// # Documenting Input Parameters
+/// To document input parameters, the following format is used:
+///```text
+/// #[add_doc(input: <input_name> - <comment> (, <optional type string>))]
+///```
+/// where <input_name> \<comment\> and (optional type string) are all string literals. See example
+/// below.
+///
+///```
+/// use bmw_err::*;
+/// use bmw_derive::*;
+///
+/// #[document]
+/// pub trait TestDoc {
+///     #[add_doc(input: "myvalue1" - "this is a comment about `myvalue1`.")]
+///     fn abc(&self, myvalue1: usize, myvalue2: bool) -> Result<String, Error>;
+///
+/// }
+///```
+/// The optional type string is used for macros where the proc_macro cannot determine anything about the
+/// type. This allows the user to specify a type. See example below.
+///```
+/// use bmw_err::*;
+/// use bmw_derive::*;
+///
+/// #[document]
+/// #[add_doc(input: values - "value to add to 1", " usize ")]
+/// #[macro_export]
+/// macro_rules! add_to_1 {
+///        ($name:expr) => {
+///                 let value = $name as usize + 1;
+///                 println!("value: {}", value);
+///        };
+/// }
+///```
+/// # Documenting Errors
+/// To document errors, the following format is used:
+///```text
+/// #[add_doc(error: <path_to_error_type> - <error comment>)]
+///```
+/// Both parameters are string literals. See example below.
+///
+///```
+/// use bmw_err::*;
+/// use bmw_derive::*;
+///
+/// #[document]
+/// pub trait mytrait {
+///     #[add_doc(error: "bmw_err::ErrKind::IO" - "if an i/o error occurs while writing")]
+///     fn write(&mut self, data: &[u8]) -> Result<(), Error>;
+/// }
+///```
+/// # Documenting Also See
+///
+/// To point users to additional documentation, `see` can be used. The format is as follows:
+///```text
+/// #[add_doc(see: <path_to_additional_documentation>)]
+///```
+/// The parameter is expected to be a string literal. See example below.
+///
+///```
+/// use bmw_err::*;
+/// use bmw_derive::*;
+///
+/// #[document]
+/// pub trait mytrait {
+///     #[add_doc(see: "bmw_log::Log::log")]
+///     fn write(&mut self, data: &[u8]) -> Result<(), Error>;
+/// }
+///```
+/// # Documenting Return
+/// To document the returned value, `return` can be used. The format is as follows:
+///```text
+/// #[add_doc(return: <comment>)]
+///```
+/// \<comment\> is expected to be a string literal. See example below.
+///```
+/// use bmw_err::*;
+/// use bmw_derive::*;
+///
+/// #[document]
+/// pub trait mytrait {
+///     #[add_doc(return: "this function returns the [`unit`] primitive")]
+///     fn write(&mut self, data: &[u8]) -> Result<(), Error>;
+/// }
+///```
+/// # Using the add_doc with other documentation
+/// The [`crate::add_doc()`] and [`crate::document()`] proc_macro_attributes work fine with other
+/// documentation. When generating documentation, by default add_doc will add it's documentation
+/// just before the function so all other documentation will appear above it, however a `doc_point`
+/// may be set to place certain documentation below the documentation that add_doc produces. See
+/// example below.
+///
+///```
+/// use bmw_err::*;
+/// use bmw_derive::*;
+///
+/// #[document]
+/// pub trait mytrait {
+///     /// This is the write function.
+///     /// more comments.
+///     /// ...
+///     #[add_doc(doc_point)]
+///     /// # Example
+///     ///```
+///     /// myvalue.write();
+///     ///```
+///     #[add_doc(see: "bmw_log::Log::log")]
+///     #[add_doc(error: "bmw_err::ErrKind::IO" - "if an i/o error occurs while writing")]
+///     #[add_doc(input: data - "data to write")]
+///     #[add_doc(return: "this function returns the [`unit`] primitive")]
+///     fn write(&mut self, data: &[u8]) -> Result<(), Error>;
+/// }
+///```
+/// In the case above, the documentation is put after the initial comments, but before the example.
+/// If the `doc_point` attribute were not placed there, the example would be above the generated
+/// documentation.
 #[proc_macro_attribute]
 pub fn add_doc(_attr: TokenStream, item: TokenStream) -> TokenStream {
 	// add doc doesn't actually change anything, it's just a marker used by the document attribute
